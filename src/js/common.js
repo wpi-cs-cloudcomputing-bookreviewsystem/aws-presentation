78b5d27c8ed3a1f31fd73f8fd2cfca8baa61c4aa
Q-Zhao <qzhao@wpi.edu>
2018-02-13 22:52:52 -0500

    login modal added

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,27 @@
 
 
 
+
+window.onclick = function(event) {
+	let loginModal = document.getElementById('loginModal')
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+}
+
+function myProfileClicked() {
+    // need to check login
+    var isLogin = false
+    if (!isLogin) {
+        let loginModal = document.getElementById("loginModal");
+        loginModal.style.display = "block";
+    }    
+}
+
+function closeLoginModal() {
+    document.getElementById('loginModal').style.display = "none";
+}
+
 function searchBook() {
 	alert("Search book clicked!")
 }
