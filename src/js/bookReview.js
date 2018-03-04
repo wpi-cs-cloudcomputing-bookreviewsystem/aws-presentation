92dc8abad5bee397721a7c3a31f38d14e222c93c
echomelodylwh <wli6@wpi.edu>
2018-03-03 20:23:22 -0500

    add review and rate book

--- src/js/bookReview.js
+++ src/js/bookReview.js
@@ -16,21 +16,24 @@ function loadBookByISBN() {
     var xmlHttp = new XMLHttpRequest();    
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            
             var responseString = JSON.parse(xmlHttp.responseText);
             var response = JSON.parse(responseString)
             var book = JSON.parse(response.content)
+            console.log(book);
+            document.getElementById("book_title").innerHTML = book.title;
+            document.getElementById("book_author").innerHTML = book.author;
+            document.getElementById("book_image").src = book.imageUrl;
+            document.getElementById("book_description").innerHTML = book.description;
+            var score = book.score;
+            document.getElementById("score").innerHTML = "Score: "+ score;
+            document.getElementById("score_star").style = "width:"+ score*10 + "%";
 
-            console.log(book)
-            document.getElementById("book_title").innerHTML = book.title
-            document.getElementById("book_author").innerHTML = book.author
-            document.getElementById("book_image").src = book.imageUrl
-            document.getElementById("book_description").innerHTML = book.description
-            
+
+
         }
     };  
     xmlHttp.open("POST", bookUrl, true);     
-    xmlHttp.send(JSON.stringify(isbn));
+    xmlHttp.send(JSON.stringify({"isbn":isbn}));
 };
 
 function thumbUp() {
@@ -54,12 +57,83 @@ function selectPage(pageNumber) {
 }
 
 function submitNewReview() {
-    alert("submitNewReview clicked!")
+    if(sessionStorage.user == null){
+        alert("You should log in at first!");
+    }else{
+        let url = new URL(window.location.href);
+        let searchParams = new URLSearchParams(url.search);
+        isbn = searchParams.get('isbn');
+        reviewText = document.getElementById("review_text").value;
+        console.log(reviewText);
+        var user = JSON.parse(sessionStorage.user);
+        var reviewUrl = "https://cejosbrm2g.execute-api.us-east-2.amazonaws.com/test/review";
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+
+                var responseString = JSON.parse(xmlHttp.responseText);
+                console.log(xmlHttp.responseText);
+                var response = JSON.parse(responseString)
+                console.log(response);
+                if (response.content == "true"){
+                    alert("You have reviewed !");
+                    loadPage();
+                }else {
+                    alert("Error: "+ response.content);
+                }
+                // var book = JSON.parse(response.content)
+                // console.log(book)
+                // document.getElementById("book_title").innerHTML = book.title
+                // document.getElementById("book_author").innerHTML = book.author
+                // document.getElementById("book_image").src = book.imageUrl
+                // document.getElementById("book_description").innerHTML = book.description
+
+            }
+        };
+        xmlHttp.open("POST", reviewUrl, true);
+        console.log(JSON.stringify({"content":reviewText, "email":user.email, "isbn":isbn}));
+        xmlHttp.send(JSON.stringify({"content":reviewText, "email":user.email, "isbn":isbn}));
+    }
 }
 
 function showReviewerDetails() {
     alert("reviewerName is selected!")
 }
+
+function rateBook(rateNumber) {
+    if(sessionStorage.user == null){
+        alert("You should log in at first!");
+    }else{
+        let url = new URL(window.location.href);
+        let searchParams = new URLSearchParams(url.search);
+        isbn = searchParams.get('isbn');
+        var user = JSON.parse(sessionStorage.user);
+
+        var rateUrl = "https://cejosbrm2g.execute-api.us-east-2.amazonaws.com/test/books/rate";
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                var responseString = JSON.parse(xmlHttp.responseText);
+                var response = JSON.parse(responseString);
+                var score = JSON.parse(response.content).score;
+                document.getElementById("score").innerHTML = "Score: "+ score;
+                document.getElementById("score_star").style = "width:"+ score*10 + "%";
+                alert("You have rated!");
+                // var book = JSON.parse(response.content)
+                // console.log(book)
+                // document.getElementById("book_title").innerHTML = book.title
+                // document.getElementById("book_author").innerHTML = book.author
+                // document.getElementById("book_image").src = book.imageUrl
+                // document.getElementById("book_description").innerHTML = book.description
+
+            }
+        };
+        xmlHttp.open("POST", rateUrl, true);
+        xmlHttp.send(JSON.stringify({"score":rateNumber, "email":user.email, "isbn":isbn}));
+    }
+}
+
+
 
 function LoadPageDefault() {
     if (sessionStorage.user == null) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 function loadPage() {
     LoadPageDefault()
     listAllBooks()
@@ -7,40 +6,65 @@ function loadPage() {
 function listAllBooks() {
     var demoUrl = "https://cejosbrm2g.execute-api.us-east-2.amazonaws.com/test/books"
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
+
+    let url = new URL(window.location.href)
+    let searchParams = new URLSearchParams(url.search);
+    page = searchParams.get('page')
+    console.log(page)
+
+    xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            
+
             var responseString = JSON.parse(xmlHttp.responseText);
             var response = JSON.parse(responseString)
-            var books = JSON.parse(response.content)
+            books = JSON.parse(response.content)
 
-            console.log(books)
-            for (i = 0; i < books.length; i++) {
+            console.log(books);
+            if (page == null) page = 1;
+            for (i = 6 * (page - 1); i < Math.min(6 * page, books.length); i++) {
                 book = books[i]
-
-                let bookTitleId = "book_" + i + "_title"
+                let bookTitleId = "book_" + (i - 6 * (page - 1))+ "_title"
                 let bookTitleElement = document.getElementById(bookTitleId);
                 bookTitleElement.innerHTML = book.title
-                bookTitleElement.href = demoUrl + "/" + book.ISBN
+                //   bookTitleElement.href = demoUrl + "/" + book.ISBN
+                bookTitleElement.href = "bookReview.html?isbn=" + book.ISBN
 
-                let bookDescriptionId = "book_" + i + "_description"
+                let bookDescriptionId = "book_" + (i - 6 * (page - 1)) + "_description"
                 let bookDescriptionElement = document.getElementById(bookDescriptionId);
                 bookDescriptionElement.innerHTML = book.description
 
 
-                let bookImageId = "book_" + i + "_image"
+                let bookImageId = "book_" + (i - 6 * (page - 1)) + "_image"
                 let bookImageElement = document.getElementById(bookImageId);
                 bookImageElement.src = book.imageUrl
                 bookImageElement.height = 420
 
-                let bookHref = "book_" + i + "_href"
+                let bookHref = "book_" + (i - 6 * (page - 1)) + "_href"
                 document.getElementById(bookHref).href = "bookReview.html?isbn=" + book.ISBN
+            }
+
+
+            var pageArea = document.getElementById("page-area");
+            for (j = 1; j <= ((books.length - 1) / 6 + 1); j++) {
+                let li = document.createElement("li");
+                li.setAttribute("class", "page-item");
+                li.setAttribute("id", "page-item-" + j);
+                let a = document.createElement("a");
+                a.setAttribute("class", "page-link")
+                a.href = "index.html?page=" + j;
+                a.innerHTML = j;
+                a.onclick = function () {
+                    listAllBooks();
+                }
+                li.appendChild(a);
+                pageArea.appendChild(li);
             }
         }
     };
     xmlHttp.open("GET", demoUrl, true);
     xmlHttp.send(null);
 }
+
 
 
 function LoadPageDefault() {
